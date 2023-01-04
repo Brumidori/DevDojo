1e79f24895b90426cb1178b790f6bfda0a8e99ad
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-04 18:49:02 -0300

    JBTMBY-9 comentarios

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,18 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
+    //comentario de uma linha
+    /*
+    comentario de multiplas linhas
+     */
+
+    /**
+     * comentario java Doc /**
+     * pode user html <h1>comentario</h1>
+     * ctl + alt + q = mostra visualizador
+     * @param args
+     */
+
     public static void main(String[] args) {
         System.out.println("Ola mundo");
     }
