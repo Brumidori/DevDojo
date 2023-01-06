c04094652ea99f53fae8f8ea6cf8f40595b08d74
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-06 15:40:38 -0300

    JBTMBY-12

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -6,10 +6,12 @@ public class Aula02TiposPrimitivos {
         //int, double, float, char, byte, short, long, boolean
         // tipo nomeVar = valor;
         int age = 10;
+        int cast = (int) 10000000000L; //força caber dentro do tipo, mas corta para caber
+        long numeroGrande =100000L;
         double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        float salarioFloat = 2500F;
         byte idadeByte = 10;
-        short idadeShort = 10;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 87; //tabela ascII
@@ -19,5 +21,6 @@ public class Aula02TiposPrimitivos {
         System.out.println("a idade e: " + age + " anos.");
         System.out.println(falso);
         System.out.println("char: " + caractere);
+        System.out.println(cast);
     }
 }
