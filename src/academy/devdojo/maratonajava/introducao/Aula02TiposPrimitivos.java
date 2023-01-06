129296e473c9a27b5df8e7e1b2afba6990f9680f
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-06 15:14:34 -0300

    JBTMBY-11-convencoes-de-variaveis - tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -6,8 +6,18 @@ public class Aula02TiposPrimitivos {
         //int, double, float, char, byte, short, long, boolean
         // tipo nomeVar = valor;
         int age = 10;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 87; //tabela ascII
+        char caractere1 = '\u0841'; //tabela unicode
+
 
         System.out.println("a idade e: " + age + " anos.");
-        //teste
+        System.out.println(falso);
+        System.out.println("char: " + caractere);
     }
 }
