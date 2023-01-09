047984b539cfdc9c7825f173b33368309cdac08c
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-09 15:00:47 -0300

    JBTMBY-29 exercicio

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -1,2 +1,43 @@
-package academy.devdojo.maratonajava.introducao;public class Aula05EstruturasCondicionais05 {
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        byte dia = 5;
+        //aceita como param char, int, byte, short, enum, String
+        switch (dia){
+            case 1:
+                System.out.println("domingo");
+                break;
+            case 2:
+                System.out.println("segunda");
+                break;
+            case 3:
+                System.out.println("terca");
+                break;
+            case 4:
+                System.out.println("quarta");
+                break;
+            case 5:
+                System.out.println("quinta");
+                break;
+            case 6:
+                System.out.println("sexta");
+                break;
+            case 7:
+                System.out.println("sabado");
+                break;
+            default:
+                System.out.println("opcao invalida"); //pode estar em qualquer lugar do código
+                break;
+        }
+
+        char sexo = 'M';
+        switch (sexo){
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+        }
+    }
 }
