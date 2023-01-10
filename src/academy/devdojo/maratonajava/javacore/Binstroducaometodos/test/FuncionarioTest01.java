d4224e2b5519729dc671fb8efaf07dac862c333e
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-10 15:41:21 -0300

    JBTMBY-57 exercicio

---
+++ src/academy/devdojo/maratonajava/javacore/Binstroducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Binstroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Binstroducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "Sanji";
+        funcionario.idade = 23;
+        funcionario.salarios = new double[]{1200, 987.20, 2000};
+
+        funcionario.imprime();
+
+    }
+}
