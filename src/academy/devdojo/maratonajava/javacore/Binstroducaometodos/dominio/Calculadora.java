11daefddccec3a04a816d664e92adb29862448a6
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-10 14:10:04 -0300

    JBTMBY-51 metodos

---
+++ src/academy/devdojo/maratonajava/javacore/Binstroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Binstroducaometodos.dominio;
+
+public class Calculadora {
+    //modificador de acesso (public), retorno (void = vazio), nome metodo
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Binstroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Binstroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Binstroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+    }
+}
