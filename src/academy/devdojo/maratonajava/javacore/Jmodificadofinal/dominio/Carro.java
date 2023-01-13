969c7224360737483cbe0fc4bac754e3798086fc
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-13 14:08:44 -0300

    JBTMBY-85 modificador Final - tipo primitivo

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadofinal/dominio/Carro.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadofinal.dominio;
+
+public class Carro {
+    private String nome;
+    //final - constante em caixa alta e separada por _
+    //nao pode ter valor alterado e geralemnte static - deve ser declarado quando inicializado
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    //pode ser inicalizado no bloco de ini, pois sempre eh executado pela JVM
+    public final String COR;
+    {
+        COR = "vermelha";
+    }
+}
