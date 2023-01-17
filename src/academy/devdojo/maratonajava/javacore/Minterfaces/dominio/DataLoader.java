c201d714186e110bb7d9d22074083d8a3fcd425d
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-17 14:24:56 -0300

    JBTMBY-100 atributos e metodos estaticos em Interface

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -8,4 +8,13 @@ public interface DataLoader {
     default void checkPermission(){
         System.out.println("Fazendo checagem de permissoes");
     };
+
+    //os atributos em interface sao public e os metodos que implementam nao podem ser mais restritivos
+
+    //eh possivel declarar variaveis (constantes)
+    public static final int MAX_DATA_SIZE = 10;
+
+    public static void retieveMaxDataSize(){
+        System.out.println("Dentro do retrieveMaxDataSize na interface");
+    }
 }
