dee0bff26d765397f961a3f6ceb86be267330319
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-17 14:59:53 -0300

    JBTMBY-102 polimorfismo

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.servico;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+
+public class CalculadoraImposto {
+    public static void calcularImpostoComputador(Computador computador){
+        System.out.println("Relatorio de imposto do computador");
+        double imposto = computador.calcularImposto();
+        System.out.println("Imposto do computador " + computador.getNome() +
+                " Valor do computador: "+ computador.getValor() +
+                ". Imposto a ser pago " +
+                imposto);
+    }
+
+    public static void calcularImpostoTomate(Tomate tomate){
+        System.out.println("Relatorio de imposto do tomate");
+        double imposto = tomate.calcularImposto();
+        System.out.println(
+                " Valor do tomate: "+ tomate.getValor() +
+                ". Imposto a ser pago " + imposto);
+    }
+}
