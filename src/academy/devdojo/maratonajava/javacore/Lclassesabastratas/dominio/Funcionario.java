61655e4c68dec00f4525f3fc1209d894e1671785
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-17 11:47:37 -0300

    JBTMBY-95 metodos abstratos

--- src/academy/devdojo/maratonajava/javacore/Lclassesabastratas/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabastratas/dominio/Funcionario.java
@@ -4,9 +4,12 @@ public abstract class Funcionario {
     protected String nome;
     protected double salario;
 
+    public abstract void calculaBonus();
+
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calculaBonus();
     }
 
     @Override
