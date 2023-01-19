d29caf458fa52f054a2cddc6c6596c946c7f3fc1
bru_midori <brunamidoriyy@yahoo.com.br>
2023-01-19 10:46:57 -0300

    JBTMBY-120 Wrappers

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+
+    byte byteP = 1;
+    short shortP = 1;
+    int intP = 1;
+    long longP = 1;
+    float floatP = 1;
+    double doubleP = 1;
+    char charP = 1;
+    boolean booleanP = false;
+
+
+    //objetos - sao referencias e nao valor
+    Byte byteW = 127;
+    Short shortW = 1;
+    Integer intW = 1;
+    Long longW = 1L;
+    Float floatW = 1F;
+    Double doubleW = 10D;
+    Character charW = 'W';
+    Boolean booleanW = false;
+}
